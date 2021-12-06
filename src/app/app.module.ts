c121b0f14feb487b5d5071702b482c2a80dab2ad
thummala <thummala@ISCN5CG9384PQN>
2021-12-06 12:56:16 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ 
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SortingComponent } from './sorting/sorting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortingComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sorting/sorting.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+@Component({
+  selector: 'app-sorting',
+  templateUrl: './sorting.component.html',
+  styleUrls: ['./sorting.component.css']
+})
+export class SortingComponent implements OnInit {
+  name: any ;
+  
+  constructor( private http: HttpClient) {  }
+  
+  ngOnInit(): void {
+  }
+  
+   
+
+  sortCards(item:string) {
+     
+          
+     this.http.get('https://localhost:44371/api/Sorting?cards='+item,{responseType: 'text'}).subscribe(Response=>{
+       this.name= Response
+     });
+   
+          
+  };
+  
+}
